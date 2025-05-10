9c9c458129d0ed71d60f979a2b923046a277179b
KaevonD <kaevon.dastpak@gmail.com>
2025-05-09 18:21:14 -0700

    Remove unnecessary comments, files, and folders

--- core/src/main/java/tc/oc/pgm/projectile/path/DifferentiableProjectilePath.java
+++
@@ -1,7 +0,0 @@
-package tc.oc.pgm.projectile.path;
-
-import org.bukkit.util.Vector;
-
-public interface DifferentiableProjectilePath extends ProjectilePath {
-    Vector getNormalizedTangent(int progress);
-}
